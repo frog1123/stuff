556859c83ea68c1c961ab74d679c9498f3c3516b
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-01-17 20:33:20 -0600

    e
    eee

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ function playAudio(){
     var playButton = document.getElementById("playButton");
     var pauseButton = document.getElementById('pauseButton');
     audio.play();
-    playButton.style.display="none";
     playButton.style.backgroundSize="100% 100%";
-    pauseButton.style.display="block"
     pauseButton.style.backgroundSize="100% 100%";
+    playButton.style.display="none";
+    pauseButton.style.display="block"
 }
 function pauseAudio(){
     var audio = document.getElementById("time-flies");
     var playButton = document.getElementById("playButton");
     var pauseButton = document.getElementById('pauseButton');
     audio.pause();
-    playButton.style.display="block";
     playButton.style.backgroundSize="100% 100%";
-    pauseButton.style.display="none"
     pauseButton.style.backgroundSize="100% 100%";
+    playButton.style.display="block";
+    pauseButton.style.display="none"
 }
