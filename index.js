6c7b5a56ad3819d6ce8bc1d73b4cff749ea1e432
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-01-17 21:27:54 -0600

    some stuff
    amogus

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // audio controls
 function playAudio(){
     var audio = document.getElementById("time-flies");
-    var playButton = document.getElementById("playButton");
-    var pauseButton = document.getElementById('pauseButton');
+    var playButton = document.getElementById("play-button");
+    var pauseButton = document.getElementById('pause-button');
     audio.play();
     playButton.style.backgroundSize="100% 100%";
     pauseButton.style.backgroundSize="100% 100%";
@@ -11,11 +11,21 @@ function playAudio(){
 }
 function pauseAudio(){
     var audio = document.getElementById("time-flies");
-    var playButton = document.getElementById("playButton");
-    var pauseButton = document.getElementById('pauseButton');
+    var playButton = document.getElementById("play-button");
+    var pauseButton = document.getElementById('pause-button');
     audio.pause();
     playButton.style.backgroundSize="100% 100%";
     pauseButton.style.backgroundSize="100% 100%";
     playButton.style.display="block";
     pauseButton.style.display="none"
+}
+function skipBack(){
+    var audio = document.getElementById("time-flies");
+    var backButton = document.getElementById("back-button");
+    audio.currentTime -= 10;
+}
+function skipForward(){
+    var audio = document.getElementById("time-flies");
+    var backButton = document.getElementById("back-button");
+    audio.currentTime += 10; 
 }
