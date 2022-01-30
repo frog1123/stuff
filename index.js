f5b45fb7b1574152401763dc337b0e364152a70f
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-01-30 14:10:29 -0600

    stuff
    yes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
- 
+ // audio controls
+function playAudio(){
+    var audio = document.getElementById("time-flies");
+    var playButton = document.getElementById("audio-play");
+    var pauseButton = document.getElementById('audio-pause');
+    audio.play();
+    playButton.style.backgroundSize="100% 100%";
+    pauseButton.style.backgroundSize="100% 100%";
+    playButton.style.display="none";
+    pauseButton.style.display="block"
+}
+function pauseAudio(){
+    var audio = document.getElementById("time-flies");
+    var playButton = document.getElementById("audio-play");
+    var pauseButton = document.getElementById('audio-pause');
+    audio.pause();
+    playButton.style.backgroundSize="100% 100%";
+    pauseButton.style.backgroundSize="100% 100%";
+    playButton.style.display="block";
+    pauseButton.style.display="none"
+}
+function skipBack(){
+    var audio = document.getElementById("time-flies");
+    var backButton = document.getElementById("audio-back");
+    audio.currentTime -= 10;
+}
+function skipForward(){
+    var audio = document.getElementById("time-flies");
+    var backButton = document.getElementById("audio-forward");
+    audio.currentTime += 10; 
+}
