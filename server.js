fff3218749ad502401e7fd69916ac801d600f003
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-02-24 20:54:34 -0600

    aa
    aa

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 PORT = process.env.PORT || 9000;
 
-app.use(express.static(path.join(__dirname, '/react-app/build')));
+app.use(express.static(path.join('react-app/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/react-app/build/index.html'));
+  res.sendFile(path.join('react-app/build/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
