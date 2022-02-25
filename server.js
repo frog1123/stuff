05666c638557aa178451176eaf1537ab0337bc04
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-02-24 19:42:57 -0600

    j
    j

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 PORT = process.env.PORT || 9000;
 
-app.use(express.static(path.join(__dirname, 'react-app/build')));
+app.use(express.static(path.join(__dirname, '/react-app/build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
