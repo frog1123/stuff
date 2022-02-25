19f2d10282dafd22866b0857ff073e91b59fcb50
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-02-24 20:32:46 -0600

    e
    e

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const http = require('http')
-const fs = require('fs')
-const express = require('express')
+const express = require('express');
+const { Server } = require('http');
+const path = require('path');
 
-const app = express();  
-PORT = process.env.PORT || 3000;
+const app = express();
+PORT = process.env.PORT || 9000;
+
+app.use(express.static(path.join(__dirname, './react-app/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-app.use(express.static(__dirname + '/'));
